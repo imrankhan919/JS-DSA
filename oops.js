d4f12fa9536b3a0dc31de021b7aed65510bad2a3
imrankhan919 <imrankhan919.ik@gmail.com>
2025-03-29 13:28:00 +0530

    oops_concepts

--- oops.js
+++ oops.js
@@ -35,52 +35,189 @@
 // Constructor Function
 
 // function BankAccount(name, balance = 0) {
-//   this.customerName = name;
+//   this.name = name;
 //   this.accountNumber = Date.now();
 //   this.balance = balance;
 
 //   this.deposit = (num) => {
-//     this.balance = this.balance + num;
+//     this.balance += num;
 //   };
-
 //   this.withdraw = (num) => {
-//     this.balance = this.balance - num;
+//     this.balance -= num;
 //   };
 // }
 
-// let kiranAccount = new BankAccount("Kiran", 10000);
-// let ankitaAccount = new BankAccount("Ankita", 20000);
-
-// kiranAccount.deposit(50000);
-// kiranAccount.withdraw(10000);
-// console.log(kiranAccount);
-// console.log(ankitaAccount);
-
-// const user = {
-//   name: "Priyanshu",
-//   email: "priyanshu@gmail.com",
-//   age: 20,
-//  printDetails: () => {
-//     console.log(`my name is ${this.name}`);
-//   },
+// BankAccount.prototype.deposit = function (num) {
+//   this.balance += num;
+// };
+// BankAccount.prototype.withdraw = function (num) {
+//   this.balance -= num;
 // };
 
-// user.printDetails();
+// let pankajAcc = new BankAccount("Pankaj", 10000);
+// let yasirAcc = new BankAccount("Yasir");
 
-// console.log(this);
+// pankajAcc.deposit(10000);
 
-// function CreateBook(name, genre, isPublished = false) {
-//   this.bookName = name;
-//   this.genre = genre;
-//   this.isPublished = isPublished;
+// console.log(pankajAcc);
+// yasirAcc.deposit(500);
+// console.log(yasirAcc);
 
-//   this.addYear = (year) => {
-//     this.publishedYear = year;
-//   };
+// console.log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// let numbers = new Array();
+
+// console.log(numbers);
+
+// Class
+
+// class BankAccount {
+//   name;
+//   balance;
+//   accountNUmber;
+
+//   constructor(name, balance = 0) {
+//     this.name = name;
+//     this.balance = balance;
+//     this.accountNUmber = Date.now();
+//   }
+
+//   deposit(num) {
+//     this.balance += num;
+//   }
+//   withdraw(num) {
+//     this.balance -= num;
+//   }
 // }
 
-// const harryPotter = new CreateBook("Prisonar of Azkaban", "Fantasy", true);
+// let ankitaAccount = new BankAccount("Ankita", 1000);
 
-// harryPotter.addYear(2000);
+// ankitaAccount.deposit(15000);
+// ankitaAccount.withdraw(8000);
 
-// console.log(harryPotter);
+// console.log(ankitaAccount);
+
+// Inheritence
+// class BankAccount {
+//   name;
+//   balance;
+//   accountNum;
+
+//   constructor(name, balance = 0) {
+//     this.name = name;
+//     this.balance = balance;
+//     this.accountNum = Date.now();
+//   }
+
+//   deposit(num) {
+//     this.balance += num;
+//   }
+//   withdraw(num) {
+//     this.balance -= num;
+//   }
+// }
+
+// class SavingsAccount extends BankAccount {
+//   limit = 100000;
+
+//   constructor(name, balance = 0) {
+//     super(name, balance);
+//   }
+// }
+
+// class CurrentAccount extends BankAccount {
+//   limit = 500000;
+
+//   constructor(name, balance = 0) {
+//     super(name, balance);
+//     this.accountNum = this.accountNum;
+//   }
+// }
+
+// let kiranAccount = new SavingsAccount("Kiran", 1000);
+
+// kiranAccount.deposit(100000000);
+// kiranAccount.withdraw(100000000);
+
+// console.log(kiranAccount);
+
+// Encapsulation
+// class BankAccount {
+//   name;
+//   #balance;
+//   accountNum;
+
+//   constructor(name, balance = 0) {
+//     this.name = name;
+//     this.#balance = balance;
+//     this.accountNum = Date.now();
+//   }
+
+//   deposit(num) {
+//     this.#balance += num;
+//   }
+//   withdraw(num) {
+//     this.#balance -= num;
+//   }
+
+//   set balance(num) {
+//     if (isNaN(num)) {
+//       throw new Error("Invalid Date Type");
+//     }
+
+//     this.#balance = num;
+//   }
+
+//   get balance() {
+//     return this.#balance;
+//   }
+// }
+
+// class SavingsAccount extends BankAccount {
+//   limit = 100000;
+
+//   constructor(name, balance = 0) {
+//     super(name, balance);
+//   }
+
+//   #takeLoan() {
+//     console.log("loan dissbursed");
+//   }
+
+//   get loan() {
+//     this.#takeLoan();
+//   }
+// }
+
+// class CurrentAccount extends BankAccount {
+//   limit = 500000;
+
+//   constructor(name, balance = 0) {
+//     super(name, balance);
+//     this.accountNum = this.accountNum;
+//   }
+// }
+
+// let pankajAcc = new SavingsAccount("Pankaj", 1000);
+
+// // pankajAcc.#balance = "Hello World";
+
+// // pankajAcc.setBalance(7000000);
+
+// // pankajAcc.takeLoan();
+// pankajAcc.loan;
+// pankajAcc.balance = 700000;
+
+// console.log(pankajAcc);
+
+// this
+// new
+// constructor function
+// class
+// super
+// extends
+// inheritence
+// prototype chaining
+// setter & getters
+// private fileds and methods
+// encapsulation
